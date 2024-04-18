2b94c399b0f6a59bb48c7c872bb58c7aa0a9f6ab
ndjana30 <davyndjana@gmail.com>
2024-04-18 17:40:27 +0200

    new commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// function pageAnimation() {
+   
+//     // var part1 = document.getElementById('hero');
+//     var part2 = document.getElementById('name');
+//     // if (part1.style.display === "flex") 
+//     // {
+//     //    part1.style.animation="slideInFromRight 1s forwards";
+//        part2.style.animation="dance 2s infinite";
+//     // }
+//    }
+   
